9272b170dbf0ffb66629810ba74ab25f7ecfc470
hikukun <hibiki.1106gg@gmail.com>
2026-08-25 16:29:18 +0900

    dog

---
+++ 20260825/Dog.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+class Dog
+{
+public:
+	string name;
+	Dog(string Name);
+	void ShowProfile();
+};
---
+++ 20260825/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include"Dog.h"
+
+int main(void)
+{
+	
+}
